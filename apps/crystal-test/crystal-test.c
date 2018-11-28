f4c8533d584f889c5c7f952d3801c551992dce62
Timofei Istomin <tim.ist@gmail.com>
2018-11-28 15:27:20 +0100

    Add the config structure, remove print callback, add comments

--- apps/crystal-test/crystal-test.c
+++ apps/crystal-test/crystal-test.c
@@ -6,19 +6,55 @@
 
 #include "sndtbl.c"
 
-static uint16_t app_have_packet;
-static uint16_t app_seqn;
-static uint16_t app_log_seqn;
-static uint16_t n_pkt_sent;
-static uint16_t n_pkt_recv;
-
 static app_t_payload t_payload;
 static app_a_payload a_payload;
 
-static int is_sink;
-
+/* How many packets the "sensor" nodes will send per epoch */
 #define PACKETS_PER_EPOCH 1
+
+/* Enable/disable the application-level logging */
 #define LOGGING 1
+
+#define MS_TO_TICKS(v) ((uint32_t)RTIMER_SECOND*(v)/1000)
+
+/* Crystal configuration structure */
+static crystal_config_t conf = {
+  .period  = MS_TO_TICKS(1500),
+  .is_sink = 0, /* set below in the initialisation code based on the node ID */
+
+  .ntx_S   = 3,
+  .w_S     = MS_TO_TICKS(12),
+  .plds_S  = sizeof(app_s_payload),
+  .ntx_T   = 3,
+  .w_T     = MS_TO_TICKS(8),
+  .plds_T  = sizeof(app_t_payload),
+  .ntx_A   = 3,
+  .w_A     = MS_TO_TICKS(8),
+  .plds_A  = sizeof(app_a_payload),
+
+  .r       = 2,
+  .y       = 2,
+  .z       = 4,
+  .x       = 6,
+  .xa      = 6,
+  
+  .ch_whitelist  = 0xFFFF, // NA
+  .enc_enable    = 0,      // NA
+  .scan_duration = 0xFF,   // NA
+};
+
+PROCESS(crystal_test, "Crystal test");
+
+static uint16_t app_have_packet;
+static uint16_t app_seqn;
+static uint16_t app_log_seqn;
+
+static uint16_t n_pkt_sent;
+static uint16_t n_pkt_recv;
+
+static int is_sink; // whether this node is Crystal sink
+
+static process_event_t EPOCH_END_EV;
 
 #if LOGGING
 struct out_pkt_record {
@@ -35,6 +71,13 @@ struct in_pkt_record {
 
 #endif //LOGGING
 
+/* Note that app_* Crystal callbacks are called in the interrupt context
+ * between Crystal slots, so they should return ASAP.
+ *
+ * The callbacks are described in crystal.h.
+ */
+
+// Pre-S phase Crystal callback
 uint8_t* app_pre_S() {
   app_have_packet = 0;
   //log_send_seqn = 0;
@@ -58,6 +101,7 @@ static inline void app_mark_acked() {
 #endif //LOGGING
 }
 
+// Post-S phase Crystal callback
 void app_post_S(int received, uint8_t* payload) {
   if (is_sink)
     return;
@@ -77,6 +121,7 @@ void app_post_S(int received, uint8_t* payload) {
 #endif // CONCURRENT_TXS
 }
 
+// Pre-T phase Crystal callback
 uint8_t* app_pre_T() {
   //log_send_acked = 0;
   if (app_have_packet) {
@@ -88,6 +133,7 @@ uint8_t* app_pre_T() {
   return NULL;
 }
 
+// Post-T, pre-A phase Crystal callback
 uint8_t* app_between_TA(int received, uint8_t* payload) {
   if (received) {
     t_payload = *(app_t_payload*)payload;
@@ -115,6 +161,7 @@ uint8_t* app_between_TA(int received, uint8_t* payload) {
   return (uint8_t*)&a_payload;
 }
 
+// Post-A phase Crystal callback
 void app_post_A(int received, uint8_t* payload) {
   // non-sink: if acked us, stop sending data
   //log_send_acked = 0;
@@ -134,32 +181,10 @@ void app_post_A(int received, uint8_t* payload) {
   }
 }
 
-
-void app_epoch_end() {}
-void app_ping() {}
-
-void app_print_logs() {
-#if LOGGING
-  int i;
-  if (is_sink) {
-    for (i=0; i<n_pkt_recv; i++) {
-      printf("B %u:%u %u %u\n", crystal_info.epoch, 
-          in_packets[i].src, 
-          in_packets[i].seqn,
-          app_log_seqn);
-      app_log_seqn ++;
-    }
-  }
-  else {
-    for (i=0; i<n_pkt_sent; i++) {
-      printf("A %u:%u %u %u\n", crystal_info.epoch, 
-          out_packets[i].seqn, 
-          out_packets[i].acked, 
-          app_log_seqn);
-      app_log_seqn ++;
-    }
-  }
-#endif //LOGGING
+// Called when Crystal goes to sleep (inactive portion of the epoch)
+void app_epoch_end() {
+  // "wake up" the main process
+  process_post(&crystal_test, EPOCH_END_EV, NULL);
 }
 
 
@@ -174,12 +199,12 @@ void app_crystal_start_done(bool success) {}
 #endif
 
 
-PROCESS(crystal_test, "Crystal test");
 AUTOSTART_PROCESSES(&crystal_test);
 PROCESS_THREAD(crystal_test, ev, data) {
   PROCESS_BEGIN();
 
   static struct etimer et;
+  EPOCH_END_EV = process_alloc_event();  
 
   is_sink = node_id == SINK_ID;
 
@@ -191,18 +216,46 @@ PROCESS_THREAD(crystal_test, ev, data) {
   PROCESS_YIELD_UNTIL(etimer_expired(&et));
 
 
-  printf("I am alive! EUI-64: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n", ds2411_id[0],ds2411_id[1],ds2411_id[2],ds2411_id[3],ds2411_id[4],ds2411_id[5],ds2411_id[6],ds2411_id[7]);
+  printf("I am alive! Node ID: %d, EUI-64: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n", node_id, ds2411_id[0],ds2411_id[1],ds2411_id[2],ds2411_id[3],ds2411_id[4],ds2411_id[5],ds2411_id[6],ds2411_id[7]);
 
-
-  crystal_config_t conf = crystal_get_config();
   conf.is_sink = is_sink;
-  conf.plds_S = sizeof(app_s_payload);
-  conf.plds_T = sizeof(app_t_payload);
-  conf.plds_A = sizeof(app_a_payload);
 
   PRINT_CRYSTAL_CONFIG(conf);
   crystal_start(&conf);
 
+#if LOGGING
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if (ev==EPOCH_END_EV) {
+      int i;
+      if (is_sink) {
+        for (i=0; i<n_pkt_recv; i++) {
+          printf("B %u:%u %u %u\n", crystal_info.epoch, 
+              in_packets[i].src, 
+              in_packets[i].seqn,
+              app_log_seqn);
+          app_log_seqn ++;
+        }
+      }
+      else {
+        for (i=0; i<n_pkt_sent; i++) {
+          printf("A %u:%u %u %u\n", crystal_info.epoch, 
+              out_packets[i].seqn, 
+              out_packets[i].acked, 
+              app_log_seqn);
+          app_log_seqn ++;
+        }
+      }
+    }
+  }
+#endif //LOGGING
+
   PROCESS_END();
 }
+
+
+
+// Deprecated callbacks
+void app_ping() {}
+void app_print_logs() {}
 
