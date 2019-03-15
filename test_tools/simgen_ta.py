11ef31e22149b9ad1617fc54c8fdff03bfb56cc0
Timofei Istomin <tim.ist@gmail.com>
2019-03-15 23:58:40 +0100

    Bugfix in simgen

--- test_tools/simgen_ta.py
+++ test_tools/simgen_ta.py
@@ -135,7 +135,7 @@ def mk_env(power, channel, sink, num_senders, n_empty, cca):
     
     if testbed in ("indriya", "fbk"):
         cflags += ["-DSTART_DELAY_SINK=40", "-DSTART_DELAY_NONSINK=20"]
-    elif testbed in ("unitn"):
+    elif testbed in ("unitn",):
         cflags += ["-DSTART_DELAY_SINK=20", "-DSTART_DELAY_NONSINK=5"]
     else:
         cflags += ["-DSTART_DELAY_SINK=0", "-DSTART_DELAY_NONSINK=0"]
